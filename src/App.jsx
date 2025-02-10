90a923ded16999d2ef09b155e1be15e8a77160dc
yogajanapalareddy <118717777+yogajanapalareddy@users.noreply.github.com>
2025-02-10 23:40:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         <h1>HI YOGANANDA JANAPALA THIS IS YOUR APP.</h1>
 	<h1>MY NGINX APP updated with an auto-build feature with a webhooks.</h1>
         <p>Learn how to code and build frontend applications.</p>
-        <p>End of your file.</p>
+        <p> Happy Coading.</p>
 	  <a
           className="App-link"
           href="https://reactjs.org"
