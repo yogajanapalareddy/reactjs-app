4c0c0dd7ec1d8895212ea9a8dc1860ca4831f9f1
yogajanapalareddy <118717777+yogajanapalareddy@users.noreply.github.com>
2025-02-10 18:07:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <h1>Welcome to My ReactJS App</h1>
         <p>This is a simple ReactJS application.</p>
-        <h1>HI YOGANANDA JANAPALA THIS IS YOUR APP.<h1>
+        <h1>HI YOGANANDA JANAPALA THIS IS YOUR APP.</h1>
         <p>Learn how to code and build frontend applications.</p>
         <p>End of your file.</p>
 	  <a
